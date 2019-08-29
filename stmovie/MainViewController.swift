32e6e29595badf950ae3ef0e35b60f45df9030ac
Akyl Zholonov <zholonov@gmail.com>
2019-08-30 01:28:04 +0600

    work in progress

--- stmovie/MainViewController.swift
+++ stmovie/MainViewController.swift
@@ -9,24 +9,16 @@
 import UIKit
 import YPImagePicker
 import AVFoundation
-import Lottie
-import AVKit
 
 class MainViewController: UIViewController {
     
     let selectButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Select and Play video", for: .normal)
+        button.setTitle("Choose video", for: .normal)
         return button
     }()
     
-    var player: AVPlayer!
-    var playerLayer: AVPlayerLayer!
-    private let foregroundView =  UIView()
-    private let maskLayer = CALayer()
-    private let foregroundLayer = CALayer()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,8 +30,6 @@ class MainViewController: UIViewController {
         selectButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
         selectButton.addTarget(self, action: #selector(selectButtonTapped), for: .touchUpInside)
-        
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -57,90 +47,20 @@ class MainViewController: UIViewController {
         let picker = YPImagePicker(configuration: config)
         picker.didFinishPicking{[unowned picker] items, _ in
             if let video = items.singleVideo {
-                self.playVideo(videoURL: video.url)
+                let vc = SimpleVideoViewController()
+                vc.videoURL = video.url
+                picker.dismiss(animated: true, completion: nil)
+                self.present(vc, animated: true, completion: nil)
+            } else {
+                picker.dismiss(animated: true, completion: nil)
             }
-            
-            picker.dismiss(animated: true, completion: nil)
         }
         
         present(picker, animated: true, completion: nil)
     }
     
- 
-    fileprivate func playVideo(videoURL: URL){
-        player = AVPlayer(url: videoURL)
-        
-        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: self.player.currentItem, queue: .main) { [weak self] _ in
-            self?.player?.seek(to: .zero)
-            self?.player?.play()
-        }
-        
-        playerLayer = AVPlayerLayer(player: player)
-        playerLayer.frame = self.view.bounds
-        
-        self.view.layer.addSublayer(playerLayer)
-        
-        player.play()
-        
-        setupViews()
-        setupLayers()
-        setupAnimations()
-    }
-
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-    
-    func setupViews() {
-        foregroundView.frame = view.bounds
-        foregroundView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        view.addSubview(foregroundView)
-    }
-    
-    func setupLayers() {
-        guard let playerItem = player.currentItem else { return }
-        
-        let synchronizedLyaer = AVSynchronizedLayer(playerItem: playerItem)
-        synchronizedLyaer.frame = view.bounds
-        foregroundView.layer.addSublayer(synchronizedLyaer)
-        
-        foregroundLayer.frame = view.bounds
-        foregroundLayer.backgroundColor = UIColor.black.cgColor
-        
-        synchronizedLyaer.addSublayer(foregroundLayer)
-        
-        maskLayer.frame = view.bounds
-        maskLayer.contentsGravity = .resizeAspect
-        maskLayer.contents = UIImage(named: "star")
-        
-        foregroundLayer.mask = maskLayer
-    }
-    
-    func setupAnimations() {
-        guard let playerItem = player.currentItem else { return }
-        
-        let duration = playerItem.asset.duration.seconds
-        let beginTime = AVCoreAnimationBeginTimeAtZero
-        let isRemovedOnCompletion = false
-        
-        let backgroundColorAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.backgroundColor))
-        backgroundColorAnimation.duration = duration
-        backgroundColorAnimation.isRemovedOnCompletion = isRemovedOnCompletion
-        backgroundColorAnimation.beginTime = beginTime
-        backgroundColorAnimation.fromValue = foregroundLayer.backgroundColor
-        backgroundColorAnimation.toValue = UIColor.clear.cgColor
-        
-        let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
-        scaleAnimation.duration = duration
-        scaleAnimation.isRemovedOnCompletion = isRemovedOnCompletion
-        scaleAnimation.beginTime = beginTime
-        scaleAnimation.fromValue = 3
-        scaleAnimation.toValue = 0
-        
-        maskLayer.add(scaleAnimation, forKey: nil)
-        foregroundLayer.add(backgroundColorAnimation, forKey: nil)
-    }
-
 }
 
--- stmovie/SimpleVideoViewController.swift
+++ stmovie/SimpleVideoViewController.swift
@@ -7,8 +7,108 @@
 //
 
 import UIKit
+import AVFoundation
+import Lottie
 import AVKit
 
 class SimpleVideoViewController: UIViewController {
+ 
+    var player: AVPlayer!
+    var playerLayer: AVPlayerLayer!
+    var videoURL: URL?
     
+    /*
+    let lottiesView: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.4)
+        return v
+    }()
+    */
+    
+    let exportButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Export", for: .normal)
+        return button
+    }()
+    
+    let lottiesCollectionView: UICollectionView = {
+        let cv =  UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout.init())
+        let layout:UICollectionViewFlowLayout = UICollectionViewFlowLayout.init()
+        layout.scrollDirection = UICollectionView.ScrollDirection.horizontal
+        cv.setCollectionViewLayout(layout, animated: true)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(lottiesCollectionView)
+        view.addSubview(exportButton)
+
+        exportButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        exportButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        exportButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        exportButton.widthAnchor.constraint(equalToConstant: 75).isActive = true
+        
+        lottiesCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        lottiesCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        lottiesCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        lottiesCollectionView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        exportButton.addTarget(self, action: #selector(handleExport), for: .touchUpInside)
+        
+        lottiesCollectionView.delegate = self
+        lottiesCollectionView.dataSource = self
+        lottiesCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        
+        playVideo(videoURL: videoURL!)
+    }
+    
+    @objc func handleExport() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    fileprivate func playVideo(videoURL: URL){
+        player = AVPlayer(url: videoURL)
+        
+        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: self.player.currentItem, queue: .main) { [weak self] _ in
+            self?.player?.seek(to: .zero)
+            self?.player?.play()
+        }
+        
+        playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        
+        let syncLayer = AVSynchronizedLayer(playerItem: player.currentItem!)
+        syncLayer.frame = CGRect(x: 15,y: 15,width: 100,height: 100)
+        //let image = UIImage(named: "star")?.cgImage
+        
+        //syncLayer.contents = image
+        
+        playerLayer.addSublayer(syncLayer)
+        
+        view.layer.addSublayer(playerLayer)
+       
+        
+        player.play()
+    }
+}
+
+extension SimpleVideoViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
 }
